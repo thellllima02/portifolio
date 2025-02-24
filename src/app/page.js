41c10e8a4cc1a6ebf5483efd4ee0afcd1ff70eb6
Hércules <116934166+thellllima02@users.noreply.github.com>
2025-02-24 01:01:03 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,11 @@ export default function Page(){
             <h1>Olá, me chamo Hércules</h1>
             <div >
                 <p className={styles.p}>Sou um desenvolvedor de software que gosta de criar soluções para ajudar a comunidade. Sempre foi muito curioso para ver como as coisas funcionam e como elas podem ser melhoradas. Sempre em busca de aprendizado. 🚀</p>
-                </div>
+                </div >
                 <Photo />
+
+                <a className={styles.a} href="/sobre">Saiba mais</a>
+
         </div>
     
     );
